b35e064379fbcc3ab43d8c05b2b31886ff1dc6ff
MarioSpore <ironmariospore@gmail.com>
2025-12-17 18:11:50 -0500

    Shortened code for missionsanity condition being set to 0 or 2

--- worlds/grinch/__init__.py
+++ worlds/grinch/__init__.py
@@ -81,7 +81,7 @@ class GrinchWorld(World):
             if "Giftsanity" in (data.location_group or []) and not self.options.giftsanity.value:
                 continue
 
-            if "Missions" in (data.location_group or []) and self.options.missionsanity.value == 0 or self.options.missionsanity.value == 2:
+            if "Missions" in (data.location_group or []) and self.options.missionsanity.value == (0, 2):
                 continue
 
             # If the region is in the list to be ignored, DON'T create the location and just continue.
