281d2bf7a017ff8583343eadf80f65c0b26dba72
MarioSpore <ironmariospore@gmail.com>
2026-02-22 09:45:40 -0500

    Comment out address that makes

--- worlds/grinch/Items.py
+++ worlds/grinch/Items.py
@@ -331,7 +331,8 @@ SLEIGH_TABLE: dict[str, GrinchItemData] = {
         IC.progression,
         [
             GrinchRamData(0x010200, binary_bit_pos=6),
-            GrinchRamData(0x0100AA, binary_bit_pos=5)],
+            # GrinchRamData(0x0100AA, binary_bit_pos=5), # Minigame trigger commented out due to sleigh parts being randomized
+        ],
     ),
     grinch_items.sleigh_parts.EXHAUST_PIPES: GrinchItemData(
         [
