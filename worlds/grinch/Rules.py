7ab88b69bb63f2d6fab40cee0b2ff99ef76dae01
MarioSpore <ironmariospore@gmail.com>
2026-04-12 22:24:39 -0400

    Removed "MC - " for Collect all Grinch Copter Blueprints location to prevent confusion

--- worlds/grinch/Rules.py
+++ worlds/grinch/Rules.py
@@ -2744,7 +2744,7 @@ rules_dict: dict[str, list[list[str]]] = {
             grinch_items.gadgets.ROCKET_SPRING,
         ],
     ],
-    "MC - Collect all Grinch Copter Blueprints": [
+    "Collect all Grinch Copter Blueprints": [
         [
             grinch_items.level_items.WL_SCOUT_CLOTHES,
             grinch_items.moves.MAX,
