c25983b515b1c08518dbb1e0a7f14b52978554da
MarioSpore <ironmariospore@gmail.com>
2026-04-20 19:10:51 -0400

    Fix fuzzer issues part 4

--- worlds/grinch/__init__.py
+++ worlds/grinch/__init__.py
@@ -176,6 +176,9 @@ class GrinchWorld(World):
             if "Mission Specific Item Locations" in data.location_group and self.options.randomize_mission_items:
                 continue
 
+            if "WL - Mayor's Villa - Hook" in location and not self.options.randomize_mission_items.value and self.options.exclude_gc.value:
+                continue
+
             if "Sleigh Parts" in data.location_group and self.options.randomize_sleigh_parts:
                 continue
 
