ccded9f212f17495316c94087492130e390389fe
MarioSpore <ironmariospore@gmail.com>
2026-07-11 00:23:54 -0400

    Drilling canoes without moving boulder
    https://youtu.be/lmMFtl-m5Hg?si=sErbj6BZr0bg8kDT&t=4307

--- worlds/grinch/Rules.py
+++ worlds/grinch/Rules.py
@@ -3244,11 +3244,21 @@ rules_dict: dict[str, list[list[str]]] = {
             grinch_items.level_items.WL_DRILL,
             grinch_items.moves.SEIZE,
         ],
+        [
+            grinch_items.events.ADVANCED_LOGIC,
+            grinch_items.level_items.WL_DRILL,
+            grinch_items.moves.PANCAKE,
+        ],
     ],
     "WL - North Shore - Drilling Holes In Canoes - Left side on top of car": [
         [
             grinch_items.level_items.WL_DRILL,
             grinch_items.moves.SEIZE,
+        ],
+        [
+            grinch_items.events.ADVANCED_LOGIC,
+            grinch_items.level_items.WL_DRILL,
+            grinch_items.moves.PANCAKE,
         ],
     ],
     "WL - Submarine World - Modifying The Marine Mobile - Outer Fast-moving Fish": [[]],
